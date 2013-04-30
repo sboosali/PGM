f97ada8139125c6d371089a949568463b55632b1
Sam Boosalis <samboosalis@gmail.com>
2013-04-30 19:27:47 -0400

    particle filter that samples from likelihood

---
+++ fin/polytrans.py
@@ -0,0 +1,105 @@
+#!/usr/bin/python
+from __future__ import division
+from sam.sam import *
+
+from numpy import *
+import numpy as np
+from matplotlib.pyplot import *
+import matplotlib.pyplot as plt
+import numpy.random as samples
+import scipy.stats as pdfs
+from scipy.io import loadmat
+import numpy.linalg
+import time
+
+def cat(*As): return concatenate(As)
+
+def mul(*As):
+    """
+    left-associate matrix multiplication with helpful dimension mismatch error message
+    """
+    prod = 1
+    for i,A in enumerate(As):
+        try:
+            prod = dot(prod,A)
+        except ValueError as e:
+            if e.message == 'matrices are not aligned':
+                print
+                print 'mul(%s)' % ('B,'*len(As[:i]) + 'A,' + '_,'*len(As[i+1:]))
+                print 'A.shape  = %s' % A.shape
+                print 'Bs.shape = %s' % prod.shape
+                print
+            raise e
+
+    return prod
+
+def coin(p=0.5): return random.random() < p
+
+def multinomial(xs, ps):
+    return xs[ argmax(samples.multinomial(1, ps)) ]
+
+def nans(shape):
+    A = empty(shape)
+    A.fill(nan)
+    return A
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+def particle_filter(Y, initial, sample, weigh, L):
+    """
+    a simple bootstrap particle filter approximately solves any state space model
+    
+    for each moment
+    particles <== sample _
+    weights <== weigh _ _
+    resample from multinomial(particles, weights)
+
+    y , data
+
+    initial
+    can sample
+    : => X
+
+    sample
+    eg p(x[t] | x[t-1])
+    can sample
+    : _ => _
+
+    weigh
+    eg p(y[t] | x[t])
+    can eval
+    : _, _ => [0,1]
+
+    L = |particles|
+
+    output X
+    T lists of L particles with d dimensions
+     """
+
+    T, p = Y.shape
+    d = initial().shape
+    X = nans((T+1,L,d))
+
+    print '--- Particle Filter ---'
+    print 'p =', p
+    print 'd =', d
+    print 'L =', L
+
+    X[0] = [initial() for _ in range(L)]
+
+    for t in range(1,T):
+        # sample
+        particles = a([sample(Y) for l in range(L)])
+
+        # weigh
+        weights = a([weigh(X[t-1], particle) for particle in particles])
+        weights /= sum(weights)
+      
+        # resample
+        X[t] = [multinomial(particles, weights) for l in range(L)]
+
+    return X
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Test
+
