92a3301f4158c9e6e3c5524f4329f0fc1ae42c82
Sam Boosalis <samboosalis@gmail.com>
2013-05-06 16:59:44 -0400

    cc. bad sampler

--- fin/polytrans.py
+++ fin/polytrans.py
@@ -1,53 +1,24 @@
-#!/usr/bin/python
+#!/usr/bin/python -W ignore::Warning
 from __future__ import division
-from sam.sam import *
-
 from numpy import *
-import numpy as np
 from matplotlib.pyplot import *
-import matplotlib.pyplot as plt
 import numpy.random as samples
 import scipy.stats as pdfs
-from scipy.io import loadmat
-import numpy.linalg
+
+import argparse
 import time
+from itertools import groupby
 
-def cat(*As): return concatenate(As)
-
-def mul(*As):
-    """
-    left-associate matrix multiplication with helpful dimension mismatch error message
-    """
-    prod = 1
-    for i,A in enumerate(As):
-        try:
-            prod = dot(prod,A)
-        except ValueError as e:
-            if e.message == 'matrices are not aligned':
-                print
-                print 'mul(%s)' % ('B,'*len(As[:i]) + 'A,' + '_,'*len(As[i+1:]))
-                print 'A.shape  = %s' % A.shape
-                print 'Bs.shape = %s' % prod.shape
-                print
-            raise e
-
-    return prod
-
-def coin(p=0.5): return random.random() < p
-
-def multinomial(xs, ps):
-    return xs[ argmax(samples.multinomial(1, ps)) ]
-
-def nans(shape):
-    A = empty(shape)
-    A.fill(nan)
-    return A
+from util import *
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
-def particle_filter(Y, initial, sample, weigh, L):
+def particle_filter(Y, X0, sample, weigh, L=100):
     """
     a simple bootstrap particle filter approximately solves any state space model
+    samples from likelihood and weighs by transition
+    Y is very informative
+    X is just for smoothness, sparsity, noise
     
     for each moment
     particles <== sample _
@@ -56,17 +27,17 @@ def particle_filter(Y, initial, sample, weigh, L):
 
     Y , data : stream
 
-    initial
+    X0
     can sample
     : => X
 
     sample
-    eg p(x[t] | x[t-1])
+    eg p(y[t] | x[t])
     can sample
     : _ => _
 
     weigh
-    eg p(y[t] | x[t])
+    eg p(x[t] | x[t-1])
     can eval
     : _, _ => [0,1]
 
@@ -76,29 +47,113 @@ def particle_filter(Y, initial, sample, weigh, L):
     T lists of L particles with d dimensions
     X[t,l,d]
     """
+    print '--- Particle Filter (L=%d) ---' % L
 
-    d = initial().shape
+    X = a(X0)
+    d, = X.shape
+    ymax = 1e6
 
-    print '--- Particle Filter ---'
-    print 'p =', p
-    print 'd =', d
-    print 'L =', L
+    for t,y in enumerate(Y):
+        print '- %d -' % (t+1)
 
-    X = [initial() for _ in range(L)]
+        ymax = max(ymax, y.max())
 
-    for y in Y:
         # sample
-        particles = a([sample(y) for _ in range(L)])
+        particles = sample(y, L, ymin=0, ymax=ymax)
 
         # weigh
         weights = a([weigh(X, particle) for particle in particles])
         weights /= sum(weights)
-      
-        # resample
-        X = [multinomial(particles, weights) for _ in range(L)]
 
+        # resample
+        Xs = a([multinomial(particles, weights) for _ in range(L)])
+
+        # infer
+        X = [max(var, key=lambda val: sum(val==Xs[:,_])) for _ in range(d)]
         yield X
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Input
+
+cmd=argparse.ArgumentParser(description='Polyphonic Transcription by Particle Filter with Likelihood Samples')
+cmd.add_argument('file', help='a .wav audio file, the input to transcribe')
+cmd.add_argument('data', help='a dir of .wav audio files, defines what notes are')
+args=cmd.parse_args()
+
+file = args.file
+window_size = 2**12 # 44100 samples/second / 2^12 samples/window = 10 windows/second
+Y, sample_rate = fft_wav(file, window_size=window_size)
+T, _ = Y.shape # time in windows
+
+A,freqs,notes = basis(args.data, truncate=44100*5, window_size=window_size)
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Model
+
+# the graphical model is a factoiral HMM
+var = [0,1]
+d,_ = A.shape
+
+L = 100
+X0 = [0]*d
+
+#TODO data-driven
+p00 = 0.1 # P(silence sticks)
+p11 = 0.9 # P(sound sticks)
+transition = a([[p00, 1-p00],
+                [1-p11, p11]])
+def weigh(prevs, currs):
+    return product([transition[prev,curr] for prev,curr in zip(prevs,currs)])
+
+def sample(y, L, ymin=1e4, ymax=+inf):
+    """
+    : audio => note
+
+    min/max energies
+    changes greatly given different audio, format, recording, processing, etc
+    smoothly between . absolute metric for low energies . relative metric for high energies
+
+    y[i]
+    fft is wrt cycles / samples/window  but  freqs are wrt cycles / second
+    (cycles / samples/window) * (1 / windows/second) * (samples / seconds) = cycles / second = Hz
+
+    highest-amplitude freqencies
+    uniq([note(i * (sample_rate / window_size))  for i in y.argsort().tolist()[::-1] if y[i]>min])
+    
+    """
+    p = len(y)
+    window_rate = sample_rate / window_size
+
+    # index = [i for i in y.argsort().tolist()[::-1] if y[i]>ymin]
+    # frequency = [(i * (sample_rate / window_size)) for i in index]
+    # amplitude = [y[i]/ymax                         for i in index]
+
+    _freqs = {i*window_rate : y[i] for i in range(p)} # y[fft basis] => y[freq basis]
+    _notes = {n : max(_freqs[f] for f in fs) / ymax # max ampl of freqs near note
+              for n,fs in groupby(sorted(_freqs),key=note)} # group freqs by note
+
+    return a([[ber(_notes[notes[j]]) for j in range(d)] for _ in range(L)])
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # Test
+
+#title = 'Polyphonic Transcription of %s by Particle Filter' % (file)
+#d2(X, freqs, sample_rate, window_size, title=title)
+def viz(x,t):
+    if t==0: axis((0,T, 1,d))
+    if t>T: axis((0,t, 1,d))
+    plot(x)
+
+print 'T =', T
+
+before = time.clock()
+X = []
+for t,x in enumerate(particle_filter(Y, X0, sample, weigh, L=1000)):
+    X.append(x)
+X = a(X).T
+after = time.clock()
+
+print 'runtime = %ds' % int(after-before)
+d2(X, freqs, notes, sample_rate, window_size, save='', 1=title)
 
---
+++ fin/util.py
@@ -0,0 +1,235 @@
+#!/usr/bin/python -W ignore::Warning
+from __future__ import division
+from numpy import *
+from matplotlib.pyplot import *
+from matplotlib import cm
+import numpy.random as samples
+import scipy.stats as pdfs
+
+from glob import glob
+import re
+from scipy.io import wavfile
+
+from sam.sam import *
+
+
+def cat(*As): return concatenate(As)
+
+def mul(*As):
+    """ left-associate matrix multiplication """
+    prod = 1
+    for i,A in enumerate(As): prod = dot(prod,A)
+    return prod
+
+def coin(p=0.5): return random.random() < p
+
+def multinomial(xs, ps):
+    return xs[ argmax(samples.multinomial(1, ps)) ]
+
+def nans(shape):
+    A = empty(shape)
+    A.fill(nan)
+    return A
+
+def uniq(seq, key=lambda x:x):
+    """
+    removes duplicates
+    key : equivalence relation
+    """
+    seen = set()
+    seen_add = seen.add
+    return [x for x in seq if key(x) not in seen and not seen_add(key(x))]
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# Piano
+
+K = 0
+F = 1
+keys = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+def make_piano():
+    """ : () => [(key, freq)] """
+    piano = [('G#0', 25.9565)]
+    piano = piano + zip(['A0', 'A#0', 'B0'], [27.5000, 29.1352, 30.8677])
+    piano = piano + [ (keys[key] + str(octave), 16.3516 * 2**(octave + key/12))
+                      for octave in r(1,7)
+                      for key in r(0,11) ]
+    piano = piano + [('C8', 4186.01)]
+    
+    piano = [(k,round(f)) for k,f in piano]
+    return piano
+piano = make_piano()
+assert ('A4', 440) == piano[49]
+
+# runtime should be const
+# distance should be log
+def note(freq): return reduce( lambda x,y: x if nearer(freq, x[F],y[F]) else y, piano)[K]
+assert note(439) == 'A4' == note(441)
+
+def freq(note): return round(dict(piano)[note.upper()])
+assert freq('a4') == 440
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# Wav
+
+def audio_wav(file, truncate=None):
+    """
+    reads audio
+    
+    >>> wavfile.write(file, *wavfile.read(file)) # : identity
+
+    test
+    
+    """
+    #  audio :: |samples| by |channels|
+    sample_rate, audio = wavfile.read(file)
+
+    #  keep first channel
+    #  keep first 2^31 samples
+    if len(audio.shape)==1:
+        nSamples, = audio.shape
+        nChannels = 1
+        audio = audio[:int32(nSamples)]
+    else:
+        nSamples, nChannels = audio.shape
+        audio = audio[:int32(nSamples), 0]
+
+    # consistent times for consistent frequencies
+    if truncate:
+        audio = audio[:truncate]
+
+    return audio, sample_rate
+
+def my_fft(audio, sample_rate, window_size=2**12):
+    """
+    does fft on audio
+
+    window_size = 2^n
+    fft on power-of-two arrays is much faster
+
+    spectrum : |windows| by window_size
+    |frequencies| = window_size
+    |windows| / 2 * |frequencies| ~ |seconds| * sample_rate
+    
+    """
+
+    hanning_window = hanning(window_size)
+    n_windows = int32(audio.size/window_size *2) # overlap windows doubles
+    spectra = zeros((n_windows, window_size/2)) # symmetric fft halves
+
+    for i in xrange(0,n_windows-1):
+        t = int32(i* window_size/2)
+        window = audio[t : t+window_size] * hanning_window # elemwise mult
+        # half to ignore symmetry => abs to get amplitude
+        spectra[i] = abs(fft.fft(window)[:window_size/2])
+
+    return spectra, sample_rate
+
+def fft_wav(file, window_size=2**12, truncate=None):
+    """
+    reads audio and does fft
+    
+    >>> file
+    >>> Y,sample_rate = fft_wav(file, window_size = 2**12)
+    >>> threshold = Y.max() * 0.01
+    >>> plots_per_second = 5
+    >>> window_size = 2**12
+    >>> T,_ = Y.shape
+    >>> for t in range(T):
+    >>>     if (t % (2*int(sample_rate/window_size) / plots_per_second)):
+    >>>         clf()
+    >>>         axis((10,window_size/2, int(Y.min()),int(Y.max())))
+    >>>         xscale('log')
+    >>>         plot(Y[t]*(Y[t]>threshold) + threshold*(Y[t]<threshold))
+    >>>         #axhline(threshold)
+    >>>         draw(); time.sleep(1/plots_per_second)
+    >>> close()
+
+    """
+    print 'fft(audio(%s))...' % file
+    audio, sample_rate = audio_wav(file, truncate=truncate)
+    return my_fft(audio, sample_rate, window_size=window_size)
+
+def file2freq(file):
+    """
+    eg 'A440.wav' => 440
+    """
+    file = basename(file)
+
+    frequency = re.match('[A-G][#b]?(?P<freq>[0-9]+)', file)
+    if frequency:
+        frequency = frequency.groupdict()['freq']
+        return int(frequency)
+
+    frequency = re.match('[0-9]+', file)
+    if frequency:
+        frequency = frequency.group()
+        return int(frequency)
+
+    note = re.sub("[^ a-zA-Z #b 0-7]", "", file)
+    if note:
+        return freq(note)
+
+    raise Exception('bad audio filename "%s"' % file)
+
+def basis(dir, truncate=44100*5, window_size=2**12):
+    """
+    input dir, a directory of audio files
+    
+    output A, the basis matrix
+    output freqs, a list of frequencies (indexed consistently wrt A)
+    output notes, the names of the notes (indexed consistently wrt A)
+    """
+
+    data = glob('%s/*.wav' % dir)
+    data.sort(key=file2freq)
+    n = len(data)
+    A = zeros((n, window_size/2)) # : note => spectrum
+    freqs = [file2freq(file) for file in data]
+    notes = [note(file2freq(file)) for file in data]
+
+    for i,file in enumerate(data):
+        spectrum, _ = fft_wav(file, truncate=truncate, window_size=window_size)
+        A[i] = sum(spectrum, axis=0) / sum(spectrum)
+
+    return A, freqs, notes
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# Plot
+
+def pp(x): print x; return x
+
+def d2(X, freqs, notes, sample_rate, window_size, title='', save=True):
+    """
+    x-axis
+    = time in seconds
+    window_rate = sample_rate / (sample_size * 2)
+    j => j / window_rate
+    
+    y-axis
+    = pitch as note (frequency in Hz)
+    i => freqs[i]
+    """
+    d, n_windows = X.shape
+    window_rate = 2 * sample_rate / window_size # windows per second
+
+    if not save: ion()
+    axes = gca()
+    axes.imshow(X, cmap=cm.bone_r, origin='lower', aspect='auto', interpolation='nearest')
+    axes.set_title(title)
+
+    axes.get_xaxis().set_major_locator(
+        LinearLocator(1 + ceil(n_windows/window_rate)))
+    axes.get_xaxis().set_major_formatter(
+        FuncFormatter(lambda x,y: '%ds' % round(x/window_rate)))
+
+    axes.get_yaxis().set_major_locator(
+                LinearLocator(2*d+1))
+    axes.get_yaxis().set_major_formatter(
+        FuncFormatter(lambda x,y: '%s' % (notes[(y-1)//2] if odd(y) else '')))
+
+    if save:
+        show()
+    else:
+        draw()
+        time.sleep(3600)
